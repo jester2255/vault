c93685176f1e3b67cd57901ee9582c22f473bbb5
TabiNylove <tabi.nylove@gmail.com>
2017-10-07 10:38:56 -0600

    more html

--- public/js/view.js
+++ public/js/view.js
@@ -1,11 +1,54 @@
-// VARIABLES
+// POST
 //=====================================
-var addItem = {
-	name: $("#newItemName").val().trim();
-	description: $("#newItemDescribe").val().trim();
+$("#addItem").on("click", function() {
+	var addItem = {
+		name: $("#newItemName").val().trim(),
+		description: $("#newItemDescribe").val().trim(),
+		lent_out: false,
+		category_id: $("#selectCategory").val()
+	}
+    // ajax post
+    $.post("/addItem", addItem, function(data) {
+        console.log(data + "item posted!");
+    });
+});
+
+$("#addCategory").on("click", function() {
+	var addCategory = {
+		name: $("#newCategoryName").val().trim()
+	}
+    // ajax post
+    $.post("/addCategory", addItem, function(data) {
+        console.log(data + "category posted!");
+    });
+});
+
+$("#lendItem").on("click", function() {
+	var lendItem = {
+		due_date: $("#dueDate").val().trim(),
+		type: "LEND",
+		item_condition: $("#itemCondition").val().trim(),
+		lendee: $("#newLendee").val().trim()
+	}
+    // ajax post
+    $.post("/lendItem", addItem, function(data) {
+        console.log(data + "lend posted!");
+    });
+});
+
+var returnItem = {
+
 }
+// GET
+//=====================================
+
+// UPDATE
+//=====================================
+$("#lendItem").on("click", function() {
+	
+})
 //=====================================
 //=====================================
 //=====================================
-//=====================================
-//=====================================
+
+
