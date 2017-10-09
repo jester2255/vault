eda400b13662b51eb60ea9512d89450dca593f43
thethatcher <thethatcher@gmail.com>
2017-10-09 17:54:09 -0600

    added some more complex routes. untested

--- routes/transaction-api-routes.js
+++ routes/transaction-api-routes.js
@@ -5,4 +5,42 @@ module.exports = function(app) {
 			res.json(dbTransaction);
 		});
 	});
+
+	app.post("/api/transactions/:item", function(req,res){
+		var transactionObj = {
+			transaction_date: req.body.transaction_date
+			,due_date: req.body.due_date
+			,type: req.body.type
+			,item_condition: req.body.item_condition
+			,lendee: req.body.lendee
+			,CategoryId: req.body.CategoryId
+			,ItemId: req.body.ItemId
+			,UserId: req.body.UserId
+		}
+
+		if (req.body.transactionType === "RETURN") {
+			db.Item.update({lent_out:false},
+				{	
+					where:{
+						id:req.params.id
+					}
+				}).then(function(dbItem){
+					console.log("item lent_out updated",dbItem)
+				}).then(db.Transaction.create(transactionObj).then(function(dbTransaction){
+					dbTransaction
+				}))
+		}
+		else if(req.body.transactionType === "LEND"){
+			db.Item.update({lent_out:true},
+				{	
+					where:{
+						id:req.params.id
+					}
+				}).then(function(dbItem){
+					console.log("item lent_out updated",dbItem)
+				}).then(db.Transaction.create(transactionObj).then(function(dbTransaction){
+					dbTransaction
+				}))
+		}
+	});
 }
