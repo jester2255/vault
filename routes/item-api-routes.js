818a5800133ed66572264e72df9cf2c02b00993c
thethatcher <thethatcher@gmail.com>
2017-10-09 17:35:14 -0600

    added basic api routes for items, categories, and transactions

---
+++ routes/item-api-routes.js
@@ -0,0 +1,41 @@
+var db = require("../models");
+module.exports = function(app) {
+  app.get("/api/items", function(req, res) {
+    db.Item.findAll({}).then(function(dbItem) {
+      res.json(dbItem);
+    });
+  });
+
+  app.get("/api/items/:id", function(req, res) {
+    db.Item.findOne({
+      where: {
+        id: req.params.id
+      },
+    }).then(function(dbItem) {
+      res.json(dbItem);
+    });
+  });
+
+  app.post("/api/item", function(req, res) {
+    var newItem = {
+      name: req.body.name
+      ,description: req.body.description
+      ,lent_out: false
+      ,CategoryId: req.body.CategoryId
+      ,UserId: req.body.UserId
+    };
+    db.Item.create(newItem).then(function(dbItem) {
+      res.json(dbItem);
+    });
+  });
+
+  app.delete("/api/Item/:id", function(req, res) {
+    db.Item.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbItem) {
+      res.json(dbItem);
+    });
+  });
+};
---
+++ routes/transaction-api-routes.js
@@ -0,0 +1,8 @@
+var db = require("../models");
+module.exports = function(app) {
+	app.get("/api/transactions", function(req,res){
+		db.Transaction.findAll({}).then(function(dbTransaction){
+			res.json(dbTransaction);
+		});
+	});
+}
